dff03314455168f24775af7783b38685d99d92bc
azizHamadi <aziz.hamadi@esprit.tn>
2018-02-08 00:29:35 +0100

    FOSUserBundle avec template

--- src/CupCakesBundle/Controller/DefaultController.php
+++ src/CupCakesBundle/Controller/DefaultController.php
@@ -8,6 +8,21 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('CupCakesBundle:Default:index.html.twig');
+        return $this->render('CupCakesBundle:Patisseuri:layout.html.twig');
+    }
+
+    public function AdminAction()
+    {
+        return $this->render('CupCakesBundle:Admin:LayoutA.html.twig');
+    }
+
+    public function FormateurAction()
+    {
+        return $this->render('CupCakesBundle:Formateur:LayoutF.html.twig');
+    }
+
+    public function clientAction()
+    {
+        return $this->render('CupCakesBundle:Client:LayoutC.html.twig');
     }
 }
